c7aae6089c79cd758aa7d55e75a841cb0ad5f273
Ben Hyrman <ben@hyr.mn>
2021-09-11 16:29:39 -0400

    Simplified away from unsafe after more testing

--- src/Counter.cs
+++ src/Counter.cs
@@ -1,7 +1,6 @@
 using Microsoft.Toolkit.HighPerformance;
 
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace nlc;
 
@@ -9,27 +8,18 @@ public static class Counter
 {
     const int BufferSize = 512 * 1024; //A "reasonable" bigger is better when reading from a really big file
     const byte Rune = (byte)'\n';
-    const int vectorSize = 256 / 8;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-    public static unsafe int CountLines(Stream stream)
+    public static int CountLines(Stream stream)
     {
         var count = 0;
         int bytesRead;
 
-        void* ptr = (void*)NativeMemory.AlignedAlloc(byteCount: BufferSize, alignment: vectorSize);
-        Span<byte> buffer = new Span<byte>(ptr, BufferSize);
+        var buffer = new Span<byte>(new byte[BufferSize]);
 
-        try
+        while ((bytesRead = stream.Read(buffer)) > 0)
         {
-            while ((bytesRead = stream.Read(buffer)) > 0)
-            {
-                count += buffer.Slice(0, bytesRead).Count(Rune);
-            }
-        }
-        finally
-        {
-            NativeMemory.AlignedFree(ptr);
+            count += buffer.Slice(0, bytesRead).Count(Rune);
         }
 
         return count;
